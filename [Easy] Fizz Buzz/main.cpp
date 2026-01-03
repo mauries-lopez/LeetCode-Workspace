e6add600ac637a6d2b4f84a2d3efc948f5bcfe37
Mauries Lopez <maurieslopez1800@gmail.com>
2026-01-03 11:12:30 +0800

    Solved commit
    Under 14:57 minutes solved. Reviewing once again

---
+++ [Easy] Fizz Buzz/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+class Solution {
+public:
+    std::vector<std::string> fizzBuzz(int n) {
+        std::vector<std::string> res = {};
+        for ( int i = 1; i <= n; i++ ){
+            if ( i % 3 == 0 ){
+                if ( i % 5 == 0 ){
+                    res.push_back("FizzBuzz");
+                } else {
+                    res.push_back("Fizz");
+                }
+            } else if ( i % 5 == 0 ){
+                res.push_back("Buzz");
+            } else {
+                res.push_back(std::to_string(i));
+            }
+        }
+        return res;
+    };
+};
+
+int main(){
+    Solution solution;
+    std::vector<std::string> ans = solution.fizzBuzz(15);
+    for ( std::string x : ans){
+        std::cout << x << std::endl;
+    };
+}
