f6052ec059b38a2ec04348b0bdc3651e5db317c9
Mauries Lopez <maurieslopez1800@gmail.com>
2026-03-01 09:45:37 +0800

    Solved commit: Easy
    Quick and easy

---
+++ [Easy] 2894. Divisible and Non-divisible Sums Difference/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    int differenceOfSums(int n, int m) {
+        int num1 = 0, num2 = 0;
+        for ( int i = 1; i <= n ; i++ ){
+            if ( i % m == 0 ){
+                num2 += i;
+            } else {
+                num1 += i;
+            }
+        }
+        return num1 - num2;
+    }
+};
+
+int main(){
+    Solution solution;
+    int res = solution.differenceOfSums(10,3);
+    cout << res << endl;
+    return 0;
+}
