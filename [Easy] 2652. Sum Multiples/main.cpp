dad26a501bbaf7e873cd0ef522f501358dda6412
Mauries Lopez <maurieslopez1800@gmail.com>
2026-03-02 16:27:51 +0800

    Solved commit: Easy
    Quick and easy

---
+++ [Easy] 2652. Sum Multiples/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    int sumOfMultiples(int n) {
+        int res = 0;
+        for ( int i = 1 ; i <= n ; i++ ){
+            if ( i % 3 == 0 || i % 5 == 0 || i % 7 == 0 ){
+                res += i;
+            }
+        }
+        return res;
+    }
+};
+
+int main(){
+    Solution solution;
+    int res = solution.sumOfMultiples(7);
+    cout << res << endl;
+    return 0;
+}
